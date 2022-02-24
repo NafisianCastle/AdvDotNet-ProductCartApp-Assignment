fab1b9ca48a69e3e7fe3c7726be9229328243eda
NafisianCastle <nafis.cseaiub@gmail.com>
2022-02-24 13:29:43 +0600

    home page modified

--- ProductCart/Controllers/HomeController.cs
+++ ProductCart/Controllers/HomeController.cs
@@ -1,10 +1,32 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using ProductCart.Models.Database;
 
 namespace ProductCart.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            var db = new product_taskEntities();
+            var data = db.Products.ToList();
+            return View(data);
+        }
+
+        public ActionResult AddToCart()
+        {
+            if (Session["cart"]== null)
+            {
+                
+            }
+            else
+            {
+
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Checkout()
         {
             return View();
         }
