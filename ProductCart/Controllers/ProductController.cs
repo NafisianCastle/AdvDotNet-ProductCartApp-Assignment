0c108f0639862e0c85cf6c6f457517c3d947fead
NafisianCastle <nafis.cseaiub@gmail.com>
2022-02-24 12:11:47 +0600

    some changes

--- ProductCart/Controllers/ProductController.cs
+++ ProductCart/Controllers/ProductController.cs
@@ -1,57 +1,63 @@
-﻿using System.Web.Mvc;
+﻿using ProductCart.Models.Database;
+using System.Linq;
+using System.Web.Mvc;
 
-namespace ProductCart.Controllers
+namespace Newstask2.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: Product
         public ActionResult Index()
         {
-            return View();
+            var db = new product_taskEntities();
+            var data = db.Products.ToList();
+            return View(data);
         }
 
-        // GET: Product/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var db = new product_taskEntities();
+            var data = ( from p in db.Products
+                                where p.Id == id 
+                                select p).FirstOrDefault();
+            return View(data);
         }
 
-        // GET: Product/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: Product/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(Product product)
         {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            if (!ModelState.IsValid) return View(product);
+            var db = new product_taskEntities();
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        // GET: Product/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var db = new product_taskEntities();
+            var product = ( from u in db.Products 
+                            where u.Id == id 
+                            select u).FirstOrDefault();
+            return View(product);
         }
 
-        // POST: Product/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(Product newProduct)
         {
             try
             {
                 // TODO: Add update logic here
-
+                var db = new product_taskEntities();
+                var oldProduct = (from u in db.Products
+                                  where u.Id == newProduct.Id
+                                  select u).FirstOrDefault();
+                db.Entry(oldProduct).CurrentValues.SetValues(newProduct);
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
@@ -60,26 +66,14 @@ namespace ProductCart.Controllers
             }
         }
 
-        // GET: Product/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
-        }
-
-        // POST: Product/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            if (!ModelState.IsValid) return RedirectToAction("Index");
+            var db = new product_taskEntities();
+            var product = (from u in db.Products where u.Id == id select u).FirstOrDefault();
+            if (product != null) db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
